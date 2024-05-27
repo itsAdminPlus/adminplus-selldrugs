fb6fdfac234c7f6c5d37105c6a943365d3ed3e74
AdminPlus <89868900+itsAdminPlus@users.noreply.github.com>
2024-05-27 07:26:59 -0400

    Update server.lua
    Added TXAdmin handler so people can't use /dealer 5 minutes before scheduled restart to avoid people powergaming.

--- server.lua
+++ server.lua
@@ -1,6 +1,17 @@
 ESX = exports["es_extended"]:getSharedObject()
+local isRestartScheduled = false
+
+AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
+    if eventData.secondsRemaining == 300 then
+        isRestartScheduled = true
+    end
+end)
 
 RegisterCommand('dealer', function(source, args, rawcommand)
+    if isRestartScheduled then
+        TriggerClientEvent('ox_lib:notify', source, {title = 'Drugs', description = 'Cannot use dealer command during scheduled restart.', duration = 5000, position = 'center-right', icon = 'pills'})
+        return
+    end
     xPlayer = ESX.GetPlayerFromId(source)
     drugToSell = {
         type = '',
@@ -43,21 +54,40 @@ end, false)
 RegisterServerEvent('stasiek_selldrugsv2:pay')
 AddEventHandler('stasiek_selldrugsv2:pay', function(drugToSell)
     xPlayer = ESX.GetPlayerFromId(source)
-    local count = xPlayer.getInventoryItem(drugToSell.type).count
-    if count >= drugToSell.count then
-        xPlayer.removeInventoryItem(drugToSell.type, drugToSell.count)
-        if Config.account == 'money' then
-            xPlayer.addMoney(drugToSell.price)
-        else
-            xPlayer.addAccountMoney(Config.account, drugToSell.price)
-        end
+    xPlayer.removeInventoryItem(drugToSell.type, drugToSell.count)
+    if Config.account == 'money' then
+        xPlayer.addMoney(drugToSell.price)
     else
-        TriggerClientEvent('ox_lib:notify', source, {title = 'Drugs', description = Config.notify.nodrugs, duration = 8000, position = 'center-right', icon = 'pills'})
+        xPlayer.addAccountMoney(Config.account, drugToSell.price)
     end
 end)
 
+--[[RegisterServerEvent('stasiek_selldrugsv2:notifycops')
+AddEventHandler('stasiek_selldrugsv2:notifycops', function(drugToSell)
+    TriggerClientEvent('stasiek_selldrugsv2:notifyPolice', -1, drugToSell.coords)
+end)
+
+ESX.RegisterServerCallback('stasiek_selldrugsv2:getPoliceCount', function(source, cb)
+    count = 0
+
+    if Config.requiredCops then 
+    for k, v in pairs(ESX.GetPlayers()) do
+        local xPlayer = ESX.GetPlayerFromId(v)
+        if xPlayer ~= nil then 
+            if xPlayer.job.name == "police" then
+                count = count + 1
+            end
+        end
+    end
+        cb(count)
+    else
+        cb(count)
+    end
+end)]]
+
 RegisterServerEvent('stasiek_selldrugsv2:notifycops')
 AddEventHandler('stasiek_selldrugsv2:notifycops', function(drugToSell)
+    -- If the player has the police job, continue with the notification.
     TriggerClientEvent('stasiek_selldrugsv2:notifyPolice', -1, drugToSell.coords)
 end)
 
